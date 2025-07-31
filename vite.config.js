51b1fa111e3456a8c8c91e6d496609542b4e3cbe
jun17183 <jun17183@naver.com>
2025-08-01 00:39:33 +0900

    build. 배포 파일 수정

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vitest/config';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   base: '/front_6th_chapter2-1/',
@@ -7,9 +7,9 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        'index': './index.html',
-        'index.basic': './index.basic.html',
-        'index.advanced': './index.advanced.html'
+        main: './index.html',
+        basic: './index.basic.html',
+        advanced: './index.advanced.html'
       }
     }
   },
