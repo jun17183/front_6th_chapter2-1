02ae5995fb72a1c440fd8fddb90cb34b04050556
jun17183 <jun17183@naver.com>
2025-07-31 23:50:02 +0900

    build. 배포 파일 수정

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,15 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: '/front_6th_chapter2-1/',
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        'index': './index.html',
+        'index.basic': './index.basic.html',
+        'index.advanced': './index.advanced.html'
+      }
+    }
+  },
   test: {
     globals: true,
     environment: 'jsdom',
