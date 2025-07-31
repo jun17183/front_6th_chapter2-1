f338843d634ed262441dd44461dcf241b7d6a99b
jun17183 <jun17183@naver.com>
2025-08-01 00:58:07 +0900

    build. 빌드 파일 수정

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,20 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/front_6th_chapter2-1/',
   plugins: [react()],
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx']
+  },
   build: {
     rollupOptions: {
       input: {
         main: './index.html',
         basic: './index.basic.html',
         advanced: './index.advanced.html'
+      },
+      output: {
+        // 확장자 자동 추가
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js'
       }
     }
   },
